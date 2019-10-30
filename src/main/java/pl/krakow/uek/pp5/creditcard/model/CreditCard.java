c029b7d26bd114bca2122e24e2e91bcac3d1bf52
Krzysztof <krzysiekskroniow@wp.pl>
2019-10-30 09:02:45 +0100

    changed

--- src/main/java/pl/krakow/uek/pp5/creditcard/model/CreditCard.java
+++ src/main/java/pl/krakow/uek/pp5/creditcard/model/CreditCard.java
@@ -19,4 +19,13 @@ public class CreditCard {
     public BigDecimal getLimit() {
         return cardLimit;
     }
+
+    public void withdraw(BigDecimal valueOf) {
+
+
+    }
+
+    public BigDecimal getCurrentBalance() {
+        return null;
+    }
 }
--- src/test/java/pl/krakow/uek/pp5/creditcard/model/CreditCardTest.java
+++ src/test/java/pl/krakow/uek/pp5/creditcard/model/CreditCardTest.java
@@ -33,4 +33,12 @@ public class CreditCardTest {
             Assert.assertTrue(true);
         }
         }
+
+        @Test
+        public void withdrawFromCard(){
+            CreditCard card= new CreditCard("1234-5678");
+            card.assignLimit(BigDecimal.valueOf(1000));
+            card.withdraw(BigDecimal.valueOf(500));
+            Assert.assertEquals(BigDecimal.valueOf(500), card.getCurrentBalance());
+        }
     }
